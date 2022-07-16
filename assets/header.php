f2742df48937a860e9c0db689b44019647f46ab2
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-16 23:33:20 +0200

    Aggiunte alcune funzioni

--- assets/header.php
+++ assets/header.php
@@ -1,3 +1,8 @@
+<?php
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+?>
 <!DOCTYPE html>
 <html>
  <head>
@@ -11,7 +16,7 @@
   <meta name="revisit-after" content="3 days">
   <meta name="author" content="Federico Cosma">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/png" href="https://syrma.fcosma.it/lol/Hiro_Keitaro.jpg">
+  <link rel="icon" type="image/png" href="https://foxcloud.fcosma.it/assets/img/icon.png">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://foxcloud.fcosma.it/assets/css/main.css">
   <link rel="stylesheet" href="https://cdn.rgbcraft.com/static/fontawesome/css/all.min.css">
@@ -19,7 +24,8 @@
  <body style="text-align: center; background: linear-gradient(#0d0d0d, #3b3a3a); background-size: cover; background-repeat: no-repeat; background-attachment: fixed; color: white" id='body'>
   <div class="w3-bar w3-text-white">
    <a href="/" class="w3-bar-item w3-button">Home</a>
-   <a href="https://foxorg.fcosma.it/chi-siamo" class="w3-bar-item w3-button">Chi Siamo</a>
-   <a href="https://foxorg.fcosma.it/progetti" class="w3-bar-item w3-button">I nostri Progetti</a>
-   <a href="/docs/v1.0#download" class="w3-bar-item w3-button w3-orange w3-hover-black w3-hover-text-orange w3-right">Download</a>
+   <a href="https://foxorg.fcosma.it/#org" class="w3-bar-item w3-button">Chi Siamo</a>
+   <a href="/#news" class="w3-bar-item w3-button">News</a>
+   <a href="/docs/" class="w3-bar-item w3-button">Documentazione</a>
+   <a href="/docs/v1.5#download" class="w3-bar-item w3-button w3-orange w3-hover-black w3-hover-text-orange w3-right">Download</a>
   </div> 
