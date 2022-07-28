519dc6d5d79c5db5428a1a512bc9bab76c90a01a
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-28 12:08:28 +0200

    File che regola i testi

---
+++ assets/js/setText.js
@@ -0,0 +1,4 @@
+document.getElementById('disc_os').style.maxWidth = (window.innerWidth - 380 - 25) + "px";
+document.getElementById('top').style.height = window.innerHeight + "px";
+window.location.href = "#top";
+document.getElementById('goAway').style.top = window.scrollY + "px";
